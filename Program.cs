5764332fff9b3e0665866edb352e45db5b0d771e
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-21 15:31:19 -0800

    add interface to display all riddles when guesses are correct
    Co-authored-by: Kevin Williams <k9w@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -11,7 +11,23 @@ public class Program
     Console.WriteLine(sphinx.GetRiddle(score));
 
     string answerGuess = Console.ReadLine();
-    Console.WriteLine(sphinx.CheckAnswer(score, answerGuess));
+    //Console.WriteLine(sphinx.CheckAnswer(score, answerGuess));
+
+    bool isCorrect = sphinx.CheckAnswer(score, answerGuess);
+
+    while (isCorrect)
+    {
+      score++;
+      Console.WriteLine("\nYour score is " + score + ".\n\nNext Riddle:\n___________\n" + sphinx.GetRiddle(score));
+      Console.WriteLine("What is your guess?\n");
+      answerGuess = Console.ReadLine();
+      isCorrect = sphinx.CheckAnswer(score, answerGuess);
+      if (score == 5)
+      {
+        break;
+      }
+    }
+    Console.WriteLine("The game is over. You got to a score of " + score + ".");
 
   }
 }
